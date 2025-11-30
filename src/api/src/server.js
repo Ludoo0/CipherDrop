ab2eb5dff0892018beee60f96f6835bb2d9b75e4
Ludo <dev.ludoo@gmail.com>
2025-11-30 14:20:09 +0100

    Quiet dotenv logs

--- src/api/src/server.js
+++ src/api/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({quiet: true});
 import {Logger} from "./Utils/Logger.js";
 
 const app = express();
