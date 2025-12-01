be3182f71d04a829d42447fc2cf1412d539ce28c
Ludo <dev.ludoo@gmail.com>
2025-12-01 16:22:14 +0100

    Add lint:fix npm script and run it

--- src/api/src/Handlers/SecretGetMessageHandler.ts
+++ src/api/src/Handlers/SecretGetMessageHandler.ts
@@ -16,7 +16,7 @@ export async function SecretGetMessageHandler(req: Request, res: Response) {
             Logger.logLevels.DEBUG,
             Logger.contexts.ROUTES,
             `Secret with id ${id} could not be found`
-        )
+        );
         return res.status(404).json({error: 'Secret not found or has expired'});
     }
 
